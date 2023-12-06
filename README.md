9745737bbb1786a819e546e47464ad0a174dd681
SuzyWellen07 <suzy.wellen07@gmail.com>
2023-12-06 00:16:06 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # API_FastApi
+
+Atividade realizada na matéria de Desenvolvimento Web IV no IFPR.
+
+```bash
+# Ex. 1 - Criar um endpoint que identificar se um número é par ou ímpar "PAR" ou "IMPAR"
+
+# Ex. 2 - Informar 3 horarios, de acordo com a cidade que usuário especificar na url (brasilia, tokyo, londres)
+GET localhost:8000/hora/tokyo
+
+# Ex. 3 - retorna o nome do dia da semana (seg, terça, ...) de acordo com o dia atual
+Endpoint: /diasemana
+```
+
+## Executando a API
+
+```bash
+# 1. criar ambiente virtual
+python3 -m venv env
+# 2. ativar ambiente virtual
+source env/bin/activate
+# 3. instalar dependências
+pip install requirements.txt
+# 4. iniciar FastApi
+uvicorn main:app --reload
+```
--- api.py
+++ api.py
@@ -1,39 +1,59 @@
 from fastapi import FastAPI
 from datetime import datetime
+import pytz
+import locale
 
+# Defina o idioma local para exibir datas em português
+locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
+
+# Crie uma instância do FastAPI
 app = FastAPI()
 
-# app é o framework fastapi
-# define uma rota ou endpoint, localhost:8000/hora
-# GET
-@app.get("/hora") # <== endpoint
-def horacerta():
-    return { 
-        "hora": datetime.now(),
-        "tz": datetime.astimezone(tz=12),
-        "local": "BR"
+# Mapeamento de cidades para fusos horários
+fusos_horarios = {
+    'BRASILIA': 'America/Sao_Paulo',
+    'TOKYO': 'Asia/Tokyo',
+    'LONDON': 'Europe/London'
+}
+
+# Ex. 1 - Criar um endpoint que identifica se um número é par ou ímpar "PAR" ou "IMPAR"
+@app.get("/tipo/{num}")
+def tipo(num: int):
+    resultado = "par" if num % 2 == 0 else "ímpar"  # Modificação: corrigir a palavra "ímpar"
+    return {"numero": num, "tipo": resultado}
+
+# Ex. 2 - Informar 3 horários, de acordo com a cidade especificada na URL (brasilia, tokyo, londres)
+# É necessário ter a biblioteca pytz instalada (pip install pytz)
+@app.get("/hora/{cidade}")
+def hora(cidade: str):
+    cidade = cidade.upper()
+    
+    if cidade not in fusos_horarios:
+        return {"error": "Cidade não encontrada ou nome incorreto."}  # Modificação: retornar um dicionário com uma chave "error"
+
+    fuso_horario = pytz.timezone(fusos_horarios[cidade])
+    data_atual = datetime.now(fuso_horario)
+    
+    horarios = {
+        "Brasilia": data_atual.strftime("%d/%m/%Y %H:%M:%S"),
+        "Tokyo": data_atual.strftime("%d/%m/%Y %H:%M:%S"),
+        "London": data_atual.strftime("%d/%m/%Y %H:%M:%S")
     }
+    
+    return horarios
 
-@app.get("/upper/{nome}") # <== endpoint
-def upper(nome):
-    return { 
-        "nome": nome,
-        "upper": nome.upper()
-    }
+# Ex. 3 - Retorna o nome do dia da semana (seg, terça, ...) de acordo com o dia atual
+@app.get("/diasemana")
+def dia():
+    # Obter a data atual
+    data = datetime.now()
+    # Formatar a data como o nome do dia da semana
+    dia_semana = data.strftime("%A")
+    
+    # Verificar se é sábado ou domingo e adicionar "-feira" para os outros dias
+    return {"dia": dia_semana if dia_semana in ['sábado', 'domingo'] else dia_semana + "-feira"}
 
-
-# Criar ao endpoint que realização a ação:
-# Exercício 1 - retornar JSON indicando se numero é 
-# "PAR" ou "IMPAR"
-
-# Exercício 2 - Informar 3 horarios, de acordo com 
-# a cidade que usuário brasilia, tokyo, londres.
-# GET localhost:8000/hora/tokyo
-
-# Ex. 3 - Dia da semana (segunda, terça, ...)
-# /diasemana
-# -> "segunda-feira"
-
-
-# iniciar o servidor web pelo terminal:
-# uvicorn main:app --reload
+# Endpoint para a raiz
+@app.get("/")
+def root():
+    return {"message": "Bem-vindo à API FastAPI"}
